922511ed1a8a07bcadddf8ec3d86d278bdd91fef
H02c4n <svtr.hozcan@gmail.com>
2022-08-09 21:46:54 +0200

    09-08 done

---
+++ javascript/class-notes/s02/app.js
@@ -0,0 +1,13 @@
+console.log("app js is running!!!");
+
+//let b = Number(prompt("Bir rakam giriniz"));
+
+//console.log(typeof b);
+
+//let x;
+
+// do {
+//   x = prompt("Enter a Number: ");
+// } while (isNaN(x));
+
+//console.log(x, x * x);
