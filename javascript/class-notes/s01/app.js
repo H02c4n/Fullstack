cc98a923d4211e093e9f2171adff0583a0c5ca70
H02c4n <svtr.hozcan@gmail.com>
2022-08-09 21:50:12 +0200

    vue first commit

---
+++ javascript/class-notes/s01/app.js
@@ -0,0 +1,9 @@
+console.log("app.js is loaded");
+
+let fname = prompt("Enter your name");
+
+console.log("Your name is:" + fname);
+
+let result2 = confirm("Are you legal?");
+
+console.log("Yes, you are" + result2);
